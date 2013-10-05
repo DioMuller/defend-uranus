02edc4e87bd169fb17984da7d472974559e3ba21
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-10-05 17:30:59 -0300

    Expanded ship selection width to fill the screen.

--- src/DefendUranus/Activities/GamePlaySetup.cs
+++ src/DefendUranus/Activities/GamePlaySetup.cs
@@ -25,11 +25,11 @@ namespace DefendUranus.Activities
         #endregion
 
         #region Attributes
-        const int _spacing = 72;
+        int _spacing;
         KeyboardWatcher _keyboard;
         Result _result;
         List<Ship> _ships;
-        int[] _option = new[] { 0, 0 };
+        int[] _option;
         int[] _shift = new[] { 0, 0 };
         bool[] _shifting = new[] { false, false };
         Dictionary<int, Vector2>[] _iconScales;
@@ -39,9 +39,6 @@ namespace DefendUranus.Activities
         public GamePlaySetup(MainGame game)
             : base(game)
         {
-            _result = new Result();
-
-            _ships = new List<Ship>();
         }
         #endregion
 
@@ -49,21 +46,38 @@ namespace DefendUranus.Activities
         protected override void Starting()
         {
             base.Starting();
-            _result.Aborted = false;
+
+            if (_result == null)
+                _result = new Result();
+            else
+                _result.Aborted = false;
         }
 
         protected override void Activating()
         {
             base.Activating();
             _keyboard = new KeyboardWatcher();
+
             // TODO: Load ships from XML
-            _ships.AddRange(new[]
+            if (_ships == null)
             {
-                new Ship { Texture = Content.Load<Texture2D>("Sprites/Avenger"), Description = "Earth Avenger" },
-                new Ship { Texture = Content.Load<Texture2D>("Sprites/Explorer"), Description = "Uranus Explorer" },
-                new Ship { Texture = Content.Load<Texture2D>("Sprites/Fatboy"), Description = "Big Fatboy" },
-                new Ship { Texture = Content.Load<Texture2D>("Sprites/Meteoroid"), Description = "Meteoroid Destroyer" },
-            });
+                _ships = new List<Ship>
+                {
+                    new Ship { Texture = Content.Load<Texture2D>("Sprites/Avenger"), Description = "Earth Avenger" },
+                    new Ship { Texture = Content.Load<Texture2D>("Sprites/Explorer"), Description = "Uranus Explorer" },
+                    new Ship { Texture = Content.Load<Texture2D>("Sprites/Fatboy"), Description = "Big Fatboy" },
+                    new Ship { Texture = Content.Load<Texture2D>("Sprites/Meteoroid"), Description = "Meteoroid Destroyer" },
+                };
+                _spacing = (int)800 / _ships.Count;
+                _result.Player1Ship = _ships[0];
+                _result.Player2Ship = _ships[_ships.Count / 2];
+            }
+
+            _option = new[] {
+                _ships.IndexOf(_result.Player1Ship),
+                _ships.IndexOf(_result.Player2Ship)
+            };
+
             _iconScales = new[]{
                 Enumerable.Range(0, _ships.Count).ToDictionary(i => i, i => Vector2.One),
                 Enumerable.Range(0, _ships.Count).ToDictionary(i => i, i => Vector2.One)
@@ -86,44 +100,18 @@ namespace DefendUranus.Activities
             {
                 Exit(_result);
             }
+
             if (_keyboard.IsPressed(Keys.Right))
-                Shift(0, left: false);
+                ShiftSelection(0, left: false);
             else if (_keyboard.IsPressed(Keys.Left))
-                Shift(0, left: true);
+                ShiftSelection(0, left: true);
 
             if (_keyboard.IsPressed(Keys.D))
-                Shift(1, left: false);
-            else if(_keyboard.IsPressed(Keys.A))
-                Shift(1, left: true);
+                ShiftSelection(1, left: false);
+            else if (_keyboard.IsPressed(Keys.A))
+                ShiftSelection(1, left: true);
 
             base.Update(gameTime);
-        }
-
-        async void Shift(int player, bool left)
-        {
-            if (_shifting[player]) return;
-
-            _shifting[player] = true;
-            var nextOption = Mod(_option[player] + (left ? 1 : -1), _ships.Count);
-
-            await TaskEx.WhenAll(
-                FloatAnimation(100, 0, _spacing * (left ? -1 : 1), v => _shift[player] = (int)v),
-                FloatAnimation(100, 2, 1, v => _iconScales[player][_option[player]] = new Vector2(v)),
-                FloatAnimation(100, 1, 2, v => _iconScales[player][nextOption] = new Vector2(v))
-            );
-
-            _shift[player] = 0;
-            _option[player] = nextOption;
-            _shifting[player] = false;
-        }
-
-        int Mod(int value, int max)
-        {
-            if (value < 0)
-                return value % max + max;
-            if (value >= max)
-                return value % max;
-            return value;
         }
 
         protected override void Draw(GameTime gameTime)
@@ -141,8 +129,10 @@ namespace DefendUranus.Activities
         {
             int sideShips = _ships.Count - 2;
             int width = (sideShips * 2 + 1) * _spacing;
+            int x = (800 - width) / 2 + _spacing / 2;
+
             var option = _option[player];
-            for (int i = option - sideShips, x = (800 - width) / 2 + 32; i <= option + sideShips; i++, x += _spacing)
+            for (int i = option - sideShips; i <= option + sideShips; i++, x += _spacing)
             {
                 int ship = Mod(i, _ships.Count);
                 SpriteBatch.Draw(_ships[ship].Texture,
@@ -151,6 +141,44 @@ namespace DefendUranus.Activities
                     scale: _iconScales[player][ship],
                     origin: new Vector2(16));
             }
+        }
+        #endregion
+
+        #region Private
+        async void ShiftSelection(int player, bool left)
+        {
+            if (_shifting[player]) return;
+            _shifting[player] = true;
+
+            var oldOption = _option[player];
+            var nextOption = Mod(_option[player] + (left ? -1 : 1), _ships.Count);
+
+            if (player == 0)
+                _result.Player1Ship = _ships[nextOption];
+            else if (player == 1)
+                _result.Player2Ship = _ships[nextOption];
+
+            await TaskEx.WhenAll(
+                // Anima deslocamento para direita/esquerda
+                FloatAnimation(200, 0, _spacing * (left ? 1 : -1), v => _shift[player] = (int)v),
+                // Anima zoom out, de nave desselecionada
+                FloatAnimation(200, 2, 1, v => _iconScales[player][oldOption] = new Vector2(v)),
+                // Anima zoom in, de nave selecionada
+                FloatAnimation(200, 1, 2, v => _iconScales[player][nextOption] = new Vector2(v))
+            );
+
+            _shift[player] = 0;
+            _shifting[player] = false;
+            _option[player] = nextOption;
+        }
+
+        int Mod(int value, int max)
+        {
+            if (value < 0)
+                return value % max + max;
+            if (value >= max)
+                return value % max;
+            return value;
         }
         #endregion
     }
