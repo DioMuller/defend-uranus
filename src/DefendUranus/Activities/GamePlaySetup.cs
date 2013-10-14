19cbff8ef52798d26998b15b0b2147606a7f33bf
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-10-13 22:39:04 -0300

    GamePlay setup code documentation.

--- src/DefendUranus/Activities/GamePlaySetup.cs
+++ src/DefendUranus/Activities/GamePlaySetup.cs
@@ -18,14 +18,30 @@ namespace DefendUranus.Activities
     class GamePlaySetup : GameActivity<GamePlaySetup.Result>
     {
         #region Nested
+        /// <summary>
+        /// Setup Result.
+        /// Contains the information selected before the game starts.
+        /// </summary>
         public class Result
         {
+            /// <summary>
+            /// True if the setup was cancelled by the user.
+            /// </summary>
             public bool Aborted { get; set; }
 
+            /// <summary>
+            /// Player 1 ship selection.
+            /// </summary>
             public ShipDescription Player1Selection { get; set; }
+            /// <summary>
+            /// Player 2 ship selection.
+            /// </summary>
             public ShipDescription Player2Selection { get; set; }
         }
 
+        /// <summary>
+        /// Information about the user selection.
+        /// </summary>
         class PlayerSelectionInfo
         {
             public int DrawShift { get; set; }
@@ -34,6 +50,9 @@ namespace DefendUranus.Activities
             public IDictionary<ShipDescription, Vector2> IconScales { get; set; }
         }
 
+        /// <summary>
+        /// Description of the ship to be used in the GamePlay.
+        /// </summary>
         public class ShipDescription
         {
             public ShipDescription(ContentManager content, string texturePath, float mass, string description)
@@ -44,6 +63,7 @@ namespace DefendUranus.Activities
                 Description = description;
             }
 
+            // TODO: Change Texture to Sprite.
             public Texture2D Texture { get; private set; }
             public string TexturePath { get; set; }
             public string Description { get; set; }
@@ -70,66 +90,101 @@ namespace DefendUranus.Activities
         public GamePlaySetup(MainGame game)
             : base(game)
         {
-            _ships = LoadShips();
-            _spacing = (int)GraphicsDevice.PresentationParameters.BackBufferWidth / _ships.Count;
+            _ships = LoadShips().ToList();
+            _spacing = GraphicsDevice.PresentationParameters.BackBufferWidth / _ships.Count;
             _result = new Result
             {
                 Player1Selection = _ships[0],
                 Player2Selection = _ships[_ships.Count / 2]
             };
+            _p1Info = new PlayerSelectionInfo { IconScales = GetIconScales(_result.Player1Selection) };
+            _p2Info = new PlayerSelectionInfo { IconScales = GetIconScales(_result.Player2Selection) };
         }
         #endregion
 
         #region Activity Life-Cycle
-        protected override void Starting()
-        {
-            base.Starting();
-        }
-
         protected override void Activating()
         {
             base.Activating();
             _p1Input = new PlayerInput(PlayerIndex.One);
             _p2Input = new PlayerInput(PlayerIndex.Two);
             _gameInput = new GameInput();
-
-            _p1Info = new PlayerSelectionInfo { IconScales = GetIconScales(_result.Player1Selection) };
-            _p2Info = new PlayerSelectionInfo { IconScales = GetIconScales(_result.Player2Selection) };
         }
         #endregion
 
         #region Game Loop
+        #region Update
+        /// <summary>
+        /// Update the current activity's state.
+        /// This method is invoked for each game loop.
+        /// </summary>
+        /// <param name="gameTime">Current game time.</param>
         protected override void Update(GameTime gameTime)
         {
-            _p1Input.Update();
-            _p2Input.Update();
-            _gameInput.Update();
+            UpdateInputs();
 
-            if (_gameInput.Cancel)
-            {
-                _result.Aborted = true;
-                Exit(_result);
-            }
-            // TODO: Start the game when both players have confirmed selection.
-            else if (_gameInput.Confirm)
-            {
-                _result.Aborted = false;
-                Exit(_result);
-            }
+            if (IsSetupComplete())
+                return;
 
-            if (_p1Input.Right)
-                ShiftSelection(_p1Info, left: false);
-            else if (_p1Input.Left)
-                ShiftSelection(_p1Info, left: true);
-
-            if (_p2Input.Right)
-                ShiftSelection(_p2Info, left: false);
-            else if (_p2Input.Left)
-                ShiftSelection(_p2Info, left: true);
+            CheckPlayerInput(_p1Info, _p1Input);
+            CheckPlayerInput(_p2Info, _p2Input);
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Update all inputs.
+        /// </summary>
+        void UpdateInputs()
+        {
+            _p1Input.Update();
+            _p2Input.Update();
+            _gameInput.Update();
+        }
+
+        /// <summary>
+        /// Check if the setup is complete.
+        /// </summary>
+        /// <returns>True if the activity was exited.</returns>
+        bool IsSetupComplete()
+        {
+            if (_gameInput.Cancel)
+            {
+                _result.Aborted = true;
+                Exit(_result);
+                return true;
+            }
+
+            // TODO: Start the game when both players have confirmed selection.
+            if (_gameInput.Confirm)
+            {
+                _result.Aborted = false;
+                Exit(_result);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks the player input and execute its actions.
+        /// </summary>
+        /// <param name="info">Player info.</param>
+        /// <param name="input">Player input.</param>
+        void CheckPlayerInput(PlayerSelectionInfo info, PlayerInput input)
+        {
+            if (input.Right)
+                ShiftSelection(info, left: false);
+            else if (input.Left)
+                ShiftSelection(info, left: true);
+        }
+        #endregion
+
+        #region Draw
+        /// <summary>
+        /// Draw the current activity to the screen.
+        /// </summary>
+        /// <param name="gameTime">Current game time.</param>
         protected override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
@@ -141,6 +196,13 @@ namespace DefendUranus.Activities
             SpriteBatch.End();
         }
 
+        /// <summary>
+        /// Draw the ship selection for the specified player at the specified position.
+        /// </summary>
+        /// <param name="gameTime">Current game time.</param>
+        /// <param name="selection">Current player selection.</param>
+        /// <param name="info">Current player info.</param>
+        /// <param name="height">The y position on the screen where the selection will be drawn.</param>
         private void DrawPlayerSelection(GameTime gameTime, ShipDescription selection, PlayerSelectionInfo info, int height)
         {
             int sideShips = _ships.Count - 2;
@@ -150,7 +212,7 @@ namespace DefendUranus.Activities
             var option = _ships.IndexOf(selection);
             for (int i = option - sideShips; i <= option + sideShips; i++, x += _spacing)
             {
-                var ship = _ships[Mod(i, _ships.Count)];
+                var ship = _ships[i.Mod(_ships.Count)];
                 SpriteBatch.Draw(ship.Texture,
                     position: new Vector2(x + info.DrawShift, height),
                     scale: info.IconScales[ship],
@@ -159,59 +221,68 @@ namespace DefendUranus.Activities
             }
         }
         #endregion
+        #endregion
 
         #region Private
+        #region Player input actions
+        /// <summary>
+        /// Changes the currently selected ship.
+        /// </summary>
+        /// <param name="info">Info of the player changing ship selection.</param>
+        /// <param name="left">True if selecting the ship to the left.</param>
         async void ShiftSelection(PlayerSelectionInfo info, bool left)
         {
             if (info.ShiftingSelection) return;
             info.ShiftingSelection = true;
 
+            // Calcula próxima nave, de acordo com direção pressionada
             var oldOption = info == _p1Info ? _result.Player1Selection : _result.Player2Selection;
-            var nextOption = _ships[Mod(_ships.IndexOf(oldOption) + (left ? -1 : 1), _ships.Count)];
+            var nextOption = _ships[(_ships.IndexOf(oldOption) + (left ? -1 : 1)).Mod(_ships.Count)];
 
+            // Troca a nave selecionada pelo jogador
             if (info == _p1Info)
                 _result.Player1Selection = nextOption;
             else
                 _result.Player2Selection = nextOption;
 
-            // Anima zoom in, de nave selecionada
-            var zoomIn = FloatAnimation(300, 1, 2, v => info.IconScales[nextOption] = new Vector2(v), easingFunction: Sinusoidal.EaseOut);
-            // Anima zoom out, de nave desselecionada
-            var zoomOut = FloatAnimation(100, 2, 1, v => info.IconScales[oldOption] = new Vector2(v));
-
-            // Anima deslocamento para direita/esquerda
-            await FloatAnimation(100, _spacing * (left ? -1 : +1), 0, v => info.DrawShift = (int)v);
-
-            await TaskEx.WhenAll(zoomIn, zoomOut);
+            // Realiza animação de transição
+            await TaskEx.WhenAll(
+                // Anima zoom in, de nave selecionada
+                FloatAnimation(300, 1, 2, v => info.IconScales[nextOption] = new Vector2(v), easingFunction: Sinusoidal.EaseOut),
+                // Anima zoom out, de nave desselecionada
+                FloatAnimation(100, 2, 1, v => info.IconScales[oldOption] = new Vector2(v)),
+                // Anima deslocamento para direita/esquerda
+                FloatAnimation(100, _spacing * (left ? -1 : +1), 0, v => info.DrawShift = (int)v)
+            );
 
             info.ShiftingSelection = false;
         }
+        #endregion
 
-        int Mod(int value, int max)
-        {
-            if (value < 0)
-                return value % max + max;
-            if (value >= max)
-                return value % max;
-            return value;
-        }
-
+        #region Initialization
+        /// <summary>
+        /// Calculates the initial scale values, based on the player selection.
+        /// </summary>
+        /// <param name="selection"></param>
+        /// <returns></returns>
         IDictionary<ShipDescription, Vector2> GetIconScales(ShipDescription selection)
         {
             return _ships.ToDictionary(i => i, i => i == selection ? new Vector2(2) : Vector2.One);
         }
 
-        List<ShipDescription> LoadShips()
+        /// <summary>
+        /// Load all the ships available to the players.
+        /// </summary>
+        /// <returns>Descriptions of each ship in the game.</returns>
+        IEnumerable<ShipDescription> LoadShips()
         {
             // TODO: Load ships from XML
-            return new List<ShipDescription>
-            {
-                new ShipDescription(Content, "Sprites/Avenger", 2, "Earth Avenger"),
-                new ShipDescription(Content, "Sprites/Explorer", 1, "Uranus Explorer"),
-                new ShipDescription(Content, "Sprites/Fatboy", 4, "Big Fatboy"),
-                new ShipDescription(Content, "Sprites/Meteoroid", 3, "Meteoroid Destroyer"),
-            };
+            yield return new ShipDescription(Content, "Sprites/Avenger", 2, "Earth Avenger");
+            yield return new ShipDescription(Content, "Sprites/Explorer", 1, "Uranus Explorer");
+            yield return new ShipDescription(Content, "Sprites/Fatboy", 4, "Big Fatboy");
+            yield return new ShipDescription(Content, "Sprites/Meteoroid", 3, "Meteoroid Destroyer");
         }
+        #endregion
         #endregion
     }
 }
---
+++ src/DefendUranus/Helpers/CommonExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefendUranus.Helpers
+{
+    public static class CommonExtensions
+    {
+        /// <summary>
+        /// Mod operation that will only return positive values.
+        /// </summary>
+        /// <param name="value">Initial value.</param>
+        /// <param name="max">Maximum result value.</param>
+        /// <returns>Value that cycles through the possible options.</returns>
+        public static int Mod(this int value, int max)
+        {
+            if (value < 0)
+                return value % max + max;
+            return value % max;
+        }
+    }
+}
