448691f79fac94075d4356a22eb789b7b297ca47
Diogo Muller <diogo.muller@live.com>
2013-11-23 14:16:01 -0200

    Change of names for the Meteoroid Special attack.

--- src/DefendUranus/Activities/GamePlaySetup.cs
+++ src/DefendUranus/Activities/GamePlaySetup.cs
@@ -360,7 +360,7 @@ namespace DefendUranus.Activities
             yield return new ShipDescription(Content, "Sprites/Avenger", "Earth Avenger", SpecialAttack("Pursuiter Missile"), mass: 2, maxSpeed: 20, fuel: TimeSpan.FromSeconds(15f), normalAmmo: 20, specialAmmo: 3) { ParticleColor = Color.Red, ParticleDuration = 500f };
             yield return new ShipDescription(Content, "Sprites/Explorer", "Uranus Explorer", SpecialAttack("Wanderer Probe"), mass: 1, maxSpeed: 10, fuel: TimeSpan.FromSeconds(10f), normalAmmo: 15, specialAmmo: 5) { ParticleColor = Color.CornflowerBlue, ParticleDuration = 500f };
             yield return new ShipDescription(Content, "Sprites/Fatboy", "Big Fatboy", SpecialAttack("Fleeing Fake"), mass: 4, maxSpeed: 40, fuel: TimeSpan.FromSeconds(40f), normalAmmo: 20, specialAmmo: 3) { ParticleColor = Color.Yellow, ParticleDuration = 700f };
-            yield return new ShipDescription(Content, "Sprites/Meteoroid", "Meteoroid Alienform", SpecialAttack("Meteoroid Seeker"), mass: 3, maxSpeed: 30, fuel: TimeSpan.FromSeconds(30f), normalAmmo: 25, specialAmmo: 2) { ParticleColor = Color.Green, ParticleDuration = 700f };
+            yield return new ShipDescription(Content, "Sprites/Meteoroid", "Meteoroid", SpecialAttack("Meteoroid Alienform"), mass: 3, maxSpeed: 30, fuel: TimeSpan.FromSeconds(30f), normalAmmo: 25, specialAmmo: 2) { ParticleColor = Color.Green, ParticleDuration = 700f };
             yield return new ShipDescription(Content, "Sprites/Chameleon", "The Chameleon", SpecialAttack("Invisibility Cloak"), mass: 1, maxSpeed: 50, fuel: TimeSpan.FromSeconds(5f), normalAmmo: 25, specialAmmo: 2) { ParticleColor = Color.White, ParticleDuration = 500f };
         }
 
@@ -372,7 +372,7 @@ namespace DefendUranus.Activities
                 { "Pursuiter Missile", DeployPursuiterMissile },
                 { "Wanderer Probe", DeployWandererProbe },
                 { "Fleeing Fake", DeployFleeingFake },
-                { "Meteoroid Seeker", DeployMeteoroidAlienform },
+                { "Meteoroid Alienform", DeployMeteoroidAlienform },
             };
 
             if (!specialCreators.ContainsKey(name))
